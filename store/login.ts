e9d5dea75861615fcdf5ea23a24ec4fa6a764c9e
moumensoliman <moumensoliman@gmail.com>
2023-10-17 13:01:08 +0300

    Create login store with API services

--- store/login.ts
+++ store/login.ts
@@ -2,40 +2,83 @@ import { defineStore } from 'pinia'
 import { useRouter } from 'vue-router'
 import { LoginType, LoginFormDataType } from '@/Types/login'
 import { loginApi } from '@/api/login'
+import { profileApi } from '@/api/profile'
+import { COOKIE_AUTH, PROFILE_ID } from '~/constants'
+import { navigateTo, useCookie } from 'nuxt/app'
 
 export const useLoginStore = defineStore('login', {
   state: () =>
     ({
-      user: {},
-      profile: {},
-      isAuthinticated: false,
-      errorMessage: '',
+      userData: {} || null,
+      isLoggedIn: false,
+      errMsg: '',
     }) as LoginType,
 
   actions: {
     signIn(payload: LoginFormDataType) {
       const { signInAPI } = loginApi()
+      const cookieAuth = useCookie(COOKIE_AUTH)
+      let { userData, isLoggedIn } = this
+
       signInAPI(payload)
         .then((res) => {
-          this.user = res?.data
-          this.isAuthinticated = true
-          const { replace } = useRouter()
-          replace('/')
+          userData = res?.data.data
+          cookieAuth.value = res.data.data.auth_token
+          isLoggedIn = true
+          if (process.client) {
+            localStorage.setItem(
+              PROFILE_ID,
+              JSON.stringify(res?.data.profiles[0].id),
+            )
+          }
+          window.location.reload()
         })
         .catch((err) => {
           if (err?.response?.data?.message) {
-            this.errorMessage = err?.response?.data?.message
+            this.errMsg = err?.response?.data?.message
           } else {
-            this.errorMessage = 'Something Went Wrong'
+            this.errMsg = 'Error, Please Recheck'
           }
         })
     },
-    setIsAuth(payload: boolean) {
-      this.isAuthinticated = payload
+    isAuth() {
+      const cookieAuth = useCookie(COOKIE_AUTH)
+      this.isLoggedIn = cookieAuth.value ? true : false
+      return this.isLoggedIn
+    },
+    getProfileData() {
+      const { profileAPI } = profileApi()
+      const cookieAuth = useCookie(COOKIE_AUTH)
+
+      if (cookieAuth.value) {
+        profileAPI()
+          .then((res) => {
+            this.userData = res?.data
+          })
+          .catch((err) => {
+            if (err?.response?.data?.message) {
+              this.errMsg = err?.response?.data?.message
+            } else {
+              this.errMsg = 'Error, Please Recheck'
+            }
+          })
+      }
     },
     logout() {
-      this.user = {}
-      this.isAuthinticated = false
+      const cookieAuth = useCookie(COOKIE_AUTH)
+      let { userData, isLoggedIn } = this
+
+      userData = null
+      cookieAuth.value = null
+      isLoggedIn = false
+      if (process.client) {
+        localStorage.removeItem(PROFILE_ID)
+      }
+      window.location.reload()
     },
+  },
+
+  getters: {
+    getUserData: (state) => state.userData,
   },
 })
