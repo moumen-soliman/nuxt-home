43d8754b69b04f0399b69d968d5800f64140d9a7
moumensoliman <moumensoliman@gmail.com>
2023-10-17 13:03:15 +0300

    Create Layout for all pages

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,35 @@
 <template>
   <div class="flex flex-col min-h-screen">
-    <main class="flex grow">
-      <h1>fffff</h1>
+    <div v-if="currentPageName === 'login'" class="login-bg"></div>
+    <Header />
+    <main class="flex grow max-w-screen-xl mx-auto px-5">
       <slot />
     </main>
   </div>
 </template>
 
-<script setup></script>
+<script lang="ts" setup>
+import { useRouter } from 'vue-router'
 
-<style scoped></style>
+const router = useRouter()
+let currentPageName = router.currentRoute.value.name
+</script>
+
+<style lang="scss" scoped>
+.login-bg {
+  background: url('https://static.standard.co.uk/2022/11/16/10/netflix-s.jpg') !important;
+  -webkit-filter: blur(4px);
+  position: fixed;
+  left: 0;
+  right: 0;
+  z-index: -1;
+  display: block;
+  /* Full height */
+  height: 100%;
+
+  /* Center and scale the image nicely */
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
