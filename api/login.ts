9addba57bc83e9e46e7340b1aad3b0fd5a0b1697
moumensoliman <moumensoliman@gmail.com>
2023-10-17 12:58:52 +0300

    Update login API service

--- api/login.ts
+++ api/login.ts
@@ -1,17 +1,20 @@
-import { useContext } from '@nuxtjs/composition-api'
-import { Context } from '@nuxt/types'
-import { NuxtAxiosInstance } from '@nuxtjs/axios'
 import { LoginFormDataType } from '~/Types/login'
-import { $axios } from '~/utils/api'
 
 export const loginApi = () => {
+  const { $axios } = useNuxtApp()
+
   const signInAPI = async (payload: LoginFormDataType) => {
     const res = await $axios.post('/login/signin', payload)
+    return res
+  }
 
+  const signOutAPI = async () => {
+    const res = await $axios.post('/logout')
     return res
   }
 
   return {
     signInAPI,
+    signOutAPI,
   }
 }
